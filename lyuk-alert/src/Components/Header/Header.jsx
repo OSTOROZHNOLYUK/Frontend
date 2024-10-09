de38d7e85902bcf445ac4a514bb343d43f2aad6f
lsoodik <ssonya2311@gmail.com>
2024-10-10 00:28:24 +0700

    delete dropdown

--- lyuk-alert/src/Components/Header/Header.jsx
+++ lyuk-alert/src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import style from "./Header.module.scss";
 import Logo from "../Logo";
-import Dropdown from "./Dropdown/Dropdown";
+import MenuButton from "../Buttons/MenuButton/MenuButton";
+
 export default function Header (){
   return (
     <div className={style.header + " " + "container" }>
       <Logo name="logo-main-orange"/>
-      <Dropdown/>
+      <MenuButton/>
     </div>
   );
 }
