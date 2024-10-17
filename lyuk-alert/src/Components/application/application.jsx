dea8a5223e3fe6a0187234ae08b79170fb1f08f8
TENSAIDESU <linn.dovakin@yandex.ru>
2024-10-17 20:47:17 +0300

    com

--- lyuk-alert/src/Components/application/application.jsx
+++ lyuk-alert/src/Components/application/application.jsx
@@ -1,14 +1,17 @@
 import Header from "../Header/Header";
 import steps from './steps.png';
 import Step from "./steps/steps";
+import './application.scss'
 
 const Application =()=> {
     return (
         <>
         <Header/>
-        <h1 className="map_title">Оставьте заявку об открытом люке</h1>
-            <p className="map_text"> Всего 3 шага – заполнение займет пару минут!</p>
-            <img src={steps} alt="steps" width="100%"/>
+        <div className="title">
+            <h1 className="apply_title">Оставьте заявку об открытом люке</h1>
+            <p className="apply_text"> Всего 3 шага – заполнение займет пару минут!</p>
+            <img className="steps" src={steps} alt="steps" width="100%"/>
+        </div>
             <Step/>
         </>
     )
