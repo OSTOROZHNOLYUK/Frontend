39f284452484969f7f708d365ee6002bcba3f077
lsoodik <ssonya2311@gmail.com>
2024-10-22 00:26:17 +0700

    remarks

--- lyuk-alert/src/Components/Header/Dropdown/Dropdown.jsx
+++ lyuk-alert/src/Components/Header/Dropdown/Dropdown.jsx
@@ -1,39 +1,54 @@
-import React, { useState } from 'react';
-import HeaderDropdown from './DropdownHeader';
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import HeaderDropdown from "./DropdownHeader";
+import { Link } from "react-router-dom";
 const Dropdown = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  // const [activeIndex, setActiveIndex] = useState(null);
 
-  const items = [
-    { title: 'Оставить заявку', content: ['Оставить заявку' ], link:["/application"]},
-    { title: 'Открытые люки в моем городе', content: ['Открытые люки в моем городе' ] },
-    { title: 'О проекте', content: ['История создания проекта', ' статистика', 'результаты' ] },
-    { title: 'Как помочь', content: ['Как помочь' ] },
-    { title: 'Полезные ресурсы', content: ['Полезные ресурсы' ] },
-    { title: 'Новости', content: ['Новости'] },
-    { title: 'Контакты',content: ['Контакты']},
-  ];
+  // const items = [
+  //   { title: 'Оставить заявку', content: ['Оставить заявку' ], link:["/application"]},
+  //   { title: 'Открытые люки в моем городе', content: ['Открытые люки в моем городе' ] },
+  //   { title: 'О проекте', content: ['История создания проекта', ' статистика', 'результаты' ] },
+  //   { title: 'Как помочь', content: ['Как помочь' ] },
+  //   { title: 'Полезные ресурсы', content: ['Полезные ресурсы' ] },
+  //   { title: 'Новости', content: ['Новости'] },
+  //   { title: 'Контакты',content: ['Контакты']},
+  // ];
 
-  const toggleItem = (index) => {
-    if (activeIndex === index) {
-      setActiveIndex(null);
-    } else {
-      setActiveIndex(index);
-    }
-  };
+  // const toggleItem = (index) => {
+  //   if (activeIndex === index) {
+  //     setActiveIndex(null);
+  //   } else {
+  //     setActiveIndex(index);
+  //   }
+  // };
   // const handleToggle = (index) => {
   //   setActiveIndex(activeIndex === index ? null : index);
   // };
 
   return (
-    <div style={{height: '100vh'}}>
-    <HeaderDropdown/>
-        <div className="dropdown container" style={{ backgroundColor: '#F86724', color: 'white', padding: '20px', height: '100%' }}>
+    <div style={{ height: "100vh" }}>
+      <HeaderDropdown />
+      <div className="dropdown container">
+        <ul className="dropdown-ul">
+          <Link to={"/application"}>
+            <li className="dropdown-li"><h2>Оставить заявку</h2></li>
+          </Link>
+          <Link to={"/map"}>
+            <li className="dropdown-li"><h2>Открытые люки <br /> в моем городе</h2></li>
+          </Link>
+          <Link to={"/news"}>
+            <li className="dropdown-li"><h2>Новости</h2></li>
+          </Link>
+        </ul>
+      </div>
+      {/* <div className="dropdown container" style={{ backgroundColor: '#F86724', color: 'white', padding: '20px', height: '100%' }}>
       <ul>
         {items.map((item, index) => (
-          <li className="title" key={index} onClick={() => toggleItem(index)} style={{ cursor: 'pointer' }}>
+          <li className="dropdown-title" key={index} onClick={() => toggleItem(index)} style={{ cursor: 'pointer' }}>
+            <div>
             {item.title}
             {activeIndex === index && <span> &darr;</span>}
+            </div>
             {activeIndex === index && (
             <div style={{ marginLeft: '20px', marginTop: '5px' }}>
              {item.content.map((line, idx, link) => (
@@ -46,13 +61,9 @@ const Dropdown = () => {
           </li>
         ))}
       </ul>
-    </div>
+    </div> */}
     </div>
   );
 };
 
 export default Dropdown;
-
-
-
-
--- lyuk-alert/src/Components/application/application.jsx
+++ lyuk-alert/src/Components/application/application.jsx
@@ -1,21 +1,19 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import steps from "./steps.png";
 import Step from "./steps/steps";
-import './application.scss'
+import "./application.scss";
 
-const Application =()=> {
-    return (
-        <>
-        <Header/>
-        <div className="title">
-            <h1 className="apply_title">Оставьте заявку об открытом люке</h1>
-            <p className="apply_text"> Всего 3 шага – заполнение займет пару минут!</p>
-            <img className="steps" src={steps} alt="steps" width="100%"/>
-        </div>
-            <Step/>
+const Application = () => {
+  return (
+    <>
+      <Header />
+      <div className="title">
+        <h1 className="apply_title">Оставьте заявку об открытом люке</h1>
+        <p className="apply_text">Всего 3 шага – заполнение займет пару минут!</p>
+      </div>
+      <Step />
       <Footer />
-        </>
-    )
-}
+    </>
+  );
+};
 export default Application;
